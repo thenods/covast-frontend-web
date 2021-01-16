fbbfc13ab1bd7cadfd3f9cf304c7eb0fc24df5ff
Yejun Lee <manaharu@naver.com>
2021-01-17 03:51:35 +0900

    style: fix codes according to the lint rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,74 +1,67 @@
-import Link from 'next/link'
-import { useState } from 'react'
-import {
-  useViewerQuery,
-  useUpdateNameMutation,
-  ViewerDocument,
-} from '../lib/viewer.graphql'
-import { initializeApollo } from '../lib/apollo'
+import Link from 'next/link';
+import { ChangeEvent, ReactElement, useCallback, useState } from 'react';
+import { useViewerQuery, useUpdateNameMutation, ViewerDocument, ViewerQuery } from '../lib/viewer.graphql';
+import { initializeApollo } from '../lib/apollo';
+import { NormalizedCacheObject } from '@apollo/client';
 
-const Index = () => {
-  const { viewer } = useViewerQuery().data!
-  const [newName, setNewName] = useState('')
-  const [updateNameMutation] = useUpdateNameMutation()
+const Index: () => ReactElement = () => {
+  const viewer = useViewerQuery().data?.viewer;
+  const [newName, setNewName] = useState('');
+  const [updateNameMutation] = useUpdateNameMutation();
 
-  const onChangeName = () => {
+  const handleChangeName: (e: ChangeEvent<HTMLInputElement>) => void = useCallback(e => {
+    setNewName(e.target.value);
+  }, []);
+
+  const handleUpdateName: () => void = useCallback(() => {
     updateNameMutation({
       variables: {
         name: newName,
       },
       //Follow apollo suggestion to update cache
       //https://www.apollographql.com/docs/angular/features/cache-updates/#update
-      update: (
-        store,
-        {
-          data: {
-            updateName: { name },
-          },
-        }
-      ) => {
+      update: (store, mutationResult) => {
         // Read the data from our cache for this query.
-        const { viewer } = store.readQuery({ query: ViewerDocument })
-        const newViewer = { ...viewer }
-        // Add our comment from the mutation to the end.
-        newViewer.name = name
-        // Write our data back to the cache.
-        store.writeQuery({ query: ViewerDocument, data: { viewer: newViewer } })
+        const queryResult = store.readQuery<ViewerQuery>({ query: ViewerDocument });
+
+        if (queryResult && mutationResult.data) {
+          const newViewer = { ...queryResult.viewer };
+          // Add our comment from the mutation to the end.
+          newViewer.name = mutationResult.data?.updateName.name;
+          // Write our data back to the cache.
+          store.writeQuery({ query: ViewerDocument, data: { viewer: newViewer } });
+        }
       },
-    })
-  }
+    });
+  }, [updateNameMutation, newName]);
 
   return (
     <div>
-      You're signed in as {viewer.name} and you're {viewer.status}. Go to the{' '}
-      <Link href="/about">
+      You are signed in as {viewer?.name} and you are {viewer?.status}. Go to the{' '}
+      <Link href='/about'>
         <a>about</a>
       </Link>{' '}
       page.
       <div>
-        <input
-          type="text"
-          placeholder="your new name..."
-          onChange={(e) => setNewName(e.target.value)}
-        />
-        <input type="button" value="change" onClick={onChangeName} />
+        <input type='text' placeholder='your new name...' onChange={handleChangeName} />
+        <input type='button' value='change' onClick={handleUpdateName} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export async function getStaticProps() {
-  const apolloClient = initializeApollo()
+export async function getStaticProps(): Promise<{ props: { initialApolloState: NormalizedCacheObject } }> {
+  const apolloClient = initializeApollo();
 
   await apolloClient.query({
     query: ViewerDocument,
-  })
+  });
 
   return {
     props: {
       initialApolloState: apolloClient.cache.extract(),
     },
-  }
+  };
 }
 
-export default Index
+export default Index;
