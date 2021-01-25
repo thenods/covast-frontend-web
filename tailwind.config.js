772931ea56007a0aaf8f9a51335a9934a01dab76
WDever <wdever0279@gmail.com>
2021-01-26 00:07:55 +0900

    feat(theme): Add colors and screens size at tailwind.config.js
    Change 'grey' into 'gray' in tailwindcss colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: ['./src/pages/**/*.ts', './src/pages/**/*.tsx', './src/components/**/*.ts', './src/components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: '375px',
+    },
+    colors: {
+      main: '#174DE5',
+      green: '#5CB940',
+      red: '#E51718',
+      white: '#FFF',
+      gray: {
+        light: '#F8F9FD',
+        DEFAULT: '#B4B4B4',
+        dark: '#7E7E7E',
+      },
+      black: '#1c1a1a',
+    },
   },
   variants: {
     extend: {},
